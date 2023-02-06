47e798fe1f0441901a04c9ae96e4e358d649b0ec
Ben Garfield <bengarfield@gmail.com>
2023-02-06 10:33:43 -0500

    Machine only holds up to 5000 per day

--- datastore/account.go
+++ datastore/account.go
@@ -13,6 +13,14 @@ func NewMemoryDS() MemoryDS {
 	return MemoryDS{map[int]dailyAccountUsage{}}
 }
 
+func (ds MemoryDS) getRemainingSupply() int {
+	used := 0
+	for _, usage := range ds.dailyUsage {
+		used += usage.amount
+	}
+	return 5000 - used
+}
+
 type WithdrawalInfo struct {
 	PersonalAccountNumber int `form:"pan"`
 	Amount                int `form:"amount"`
@@ -36,13 +44,19 @@ func (e ErrDailyAmount) Error() string {
 	if e.remainingAmount > 0 {
 		return fmt.Sprintf("You may only withdraw up to $%d more today from our ATM.", e.remainingAmount)
 	}
-	return "You may only withdraw up to $1000 per day from our ATM and are at the limit for today."
+	return "You may only withdraw up to $1000 per day from our ATM."
 }
 
 type ErrTooLarge struct{}
 
 func (e ErrTooLarge) Error() string {
 	return "You may only withdraw up to $500 in a single transaction."
+}
+
+type ErrATMSupply struct{}
+
+func (e ErrATMSupply) Error() string {
+	return "Sorry, this ATM does not have sufficient cash to complete your request. Please visit another location."
 }
 
 func (ds MemoryDS) Withdraw(_ context.Context, info WithdrawalInfo) error {
@@ -57,6 +71,9 @@ func (ds MemoryDS) Withdraw(_ context.Context, info WithdrawalInfo) error {
 	}
 	if info.Amount > 500 {
 		return ErrTooLarge{}
+	}
+	if info.Amount > ds.getRemainingSupply() {
+		return ErrATMSupply{}
 	}
 	ds.dailyUsage[info.PersonalAccountNumber] = dailyAccountUsage{
 		amount:      usage.amount + info.Amount,
